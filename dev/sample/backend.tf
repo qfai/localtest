b77d978e85358b3b379c3f54757500e64504da5d
qfai <404630507@qq.com>
2024-08-06 15:59:10 +0800

    Create files

--- dev/sample/backend.tf
+++ dev/sample/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<ResourceGroupName>"
-    storage_account_name = "<StorageAccountName>"
-    container_name       = "<StorageContainerName>"
+    resource_group_name  = "backend"
+    storage_account_name = "terrbackend"
+    container_name       = "backend"
     key                  = "sample.tfstate"
+    use_azuread_auth     = true
+    subscription_id      = "586c20df-c465-4f10-8673-65aa4859e7ca"
   }
 }
