c35d0c6729ce52ff7334c4a1dceb0059212c6021
qfai <quke@microsoft.com>
2024-08-09 14:49:27 +0800

    update

--- modules/hciakss/checks.tf
+++ modules/hciakss/checks.tf
@@ -1,21 +1,10 @@
-# locals {
-#   os_type = trimspace(chomp(shell("uname -s || echo Windows")))
-# }
-
-# data "external" "lnetIpCheck" {
-#   program = local.os_type == "Linux" ? ["bash", "${abspath(path.module)}/scripts/ip-range-overlap.sh", var.startingAddress, var.endingAddress, var.lnet-startingAddress, var.lnet-endingAddress] :
-#             ["powershell", "-File", "${abspath(path.module)}/scripts/ip-range-overlap.ps1", var.startingAddress, var.endingAddress, var.lnet-startingAddress, var.lnet-endingAddress]
-
-#   lifecycle {
-#     postcondition {
-#       condition     = self.result.result == "ok"
-#       error_message = "AKS Arc IP range overlaps with HCI IP range."
-#     }
-#   }
-# }
+locals {
+  is_windows = length(regexall("^[a-z]:", lower(abspath(path.root)))) > 0
+  program = local.is_windows ? "powershell.exe" : "pwsh"
+}
 
 data "external" "lnetIpCheck" {
-  program = ["pwsh", "-File", "${abspath(path.module)}/scripts/ip-range-overlap.ps1", var.startingAddress, var.endingAddress, var.lnet-startingAddress, var.lnet-endingAddress]
+  program = [local.program, "-File", "${abspath(path.module)}/scripts/ip-range-overlap.ps1", var.startingAddress, var.endingAddress, var.lnet-startingAddress, var.lnet-endingAddress]
 
   lifecycle {
     postcondition {
