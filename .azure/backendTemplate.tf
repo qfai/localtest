1ff05ea32583f83dfded7424c3b64c7c3c44c006
qfai <404630507@qq.com>
2024-08-27 11:25:19 +0800

    Create files

--- .azure/backendTemplate.tf
+++ .azure/backendTemplate.tf
@@ -5,6 +5,6 @@ terraform {
     container_name       = "tfbackend"
     key                  = "{{.GroupName}}.tfstate"
     use_azuread_auth     = true
-    subscription_id      = "de3c4d5e-af08-451a-a873-438d86ab6f4b"
+    subscription_id      = "586c20df-c465-4f10-8673-65aa4859e7ca"
   }
 }
